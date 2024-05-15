7811ef75be471b7435e8e379174d2c0b85085d69
Noah Rosenfield <nrosenfi@gmail.com>
2024-05-15 15:46:21 -0400

    adds main scene. adds CardDisplayer component and CardDesignLibrary scriptable object. adds Main as game entrypoint

---
+++ Assets/Scripts/CardDesignLibrary.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "CardDesignLibrary", menuName = "ScriptableObjects/CardDesignLibrary", order = 1)]
+
+
+/// <summary>
+/// Card faces and backings have been implemented via a ScriptableObject to leverage the Editor's drag/drop and serialization functionality.
+/// This allows new faces and backings to be added without adjusting the code.
+/// Designers should be careful to not duplicate materials in these arrays, unless specifically intentional (to increase the likelihood of cards having a particular face)
+/// </summary>
+public class CardDesignLibrary : ScriptableObject
+{
+    [SerializeField] private Material[] faces;
+    [SerializeField]
+    public Material[] Faces
+    {
+        get
+        {
+            return (Material[])faces.Clone();
+        }
+    }
+    [SerializeField] private Material[] backings;
+}
---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+
+    [SerializeField] private int numCardPairs;
+
+    void Awake()
+    {
+
+    }
+
+    public void ResetGameOnClick()
+    {
+
+    }
+
+    private void ResetGame()
+    {
+
+    }
+}
